04cc5c6b009d0ebbe080850501465f1fdc3ecdc6
Marko Lohert <lohert.marko@gmail.com>
2023-05-24 10:23:06 +0200

    RestaurantMenu is a default page that is opened when app starts.

---
+++ SeafoodSharp/SeafoodSharp/Client/Pages/NewReview.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using SeafoodSharp.Client.Analyzer;
+using SeafoodSharp.Shared;
+
+namespace SeafoodSharp.Client.Pages;
+
+public partial class NewReview
+{
+    Review CustomersReview { get; set; } = new();
+
+    string ResultAnalyzeCustomersReview { get; set; } = string.Empty;
+
+    public async Task SubmitReview()
+    {
+        ResultAnalyzeCustomersReview = ReviewAnalyzer.AnalyzeReviewMessage(CustomersReview);
+
+        await Http.PostAsJsonAsync<Review>("Review", CustomersReview);
+    }
+}
