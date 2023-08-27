9e44f5d1bd9fb811a9ee8382ef282e73a3b82d80
Marko Lohert <lohert.marko@gmail.com>
2023-08-27 16:19:51 +0200

    Added unit test project SeafoodSharp.Shared.UnitTests

---
+++ SeafoodSharp/SeafoodSharp/UnitTests/SeafoodSharp.Shared.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SeafoodSharp.Shared.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
